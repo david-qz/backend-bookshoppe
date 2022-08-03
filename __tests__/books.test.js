f85f9284fc7561262154f354374eda73f7c08d72
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-02 17:00:16 -0700

    Test and implement POST /books
    This commit handles both with and without sending author ids.

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -40,6 +40,44 @@ describe('books route', () => {
     });
   });
 
+  it('POST /books should add a new book', async () => {
+    const book = {
+      title: 'The Silmarillion',
+      released: 1977
+    };
+    const resp = await request(app).post('/books').send(book);
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number),
+      authors: expect.any(Array)
+    });
+  });
+
+  it('POST /books with authorIds should make a new books and associate it with sent author ids', async () => {
+    const book = {
+      title: 'The Silmarillion',
+      released: 1977,
+      authorIds: [1]
+    };
+    const resp = await request(app).post('/books').send(book);
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number),
+      authors: expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          name: expect.any(String),
+          dob: expect.any(String),
+          pob: expect.any(String)
+        })
+      ])
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
